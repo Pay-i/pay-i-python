1aa405f0024a9e7015b58b94fcad11a016d32571
Doron Holan <doron@pay-i.com>
2025-01-11 21:05:14 -0800

    api: manual updates

--- src/payi/lib/helpers.py
+++ src/payi/lib/helpers.py
@@ -1,9 +1,8 @@
 # Step 1: Define the new methods outside of the Payi class definition
 from typing import Dict, List, Union
 
-
 def create_limit_header_from_ids(limit_ids: List[str]) -> Dict[str, str]:
-    if not isinstance(budglimit_idset_ids, list): # type: ignore
+    if not isinstance(limit_ids, list): # type: ignore
         raise TypeError("limit_ids must be a list")
 
     valid_ids = [id.strip() for id in limit_ids if isinstance(id, str) and id.strip()] # type: ignore
@@ -23,6 +22,7 @@ def create_headers(
     request_tags: Union[List[str], None] = None,
     user_id: Union[str, None] = None,
     experience_id: Union[str, None] = None, 
+    experience_name: Union[str, None] = None, 
     ) -> Dict[str, str]:
     headers: Dict[str, str] = {}
 
@@ -34,5 +34,7 @@ def create_headers(
         headers.update({"xProxy-User-ID": user_id})
     if experience_id:
         headers.update({"xProxy-Experience-Id": experience_id})
+    if experience_name:
+        headers.update({"xProxy-Experience-Name": experience_name})
 
     return headers
